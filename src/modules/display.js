2ddcb93e13d0667fe207174382c20a3bea40d7a6
john <kiokoj81@gmail.com>
2022-10-18 16:00:07 +0300

    Added the display.js file

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,2 +1,20 @@
+const scores = [
+  { Name: "John", score: 100 },
+  { Name: "kioko", score: 79 },
+  { Name: "Mutisya", score: 70 },
+  { Name: "Engineer", score: 85 },
+];
 
+const scoresData = document.getElementById("user-data");
 
+let userData = "";
+
+scores.forEach((score) => {
+  userData += `
+  <h4 class="user-score">${score.Name} : ${score.score}</h4>
+`;
+
+  console.log(score.Name);
+});
+
+scoresData.innerHTML = userData;
