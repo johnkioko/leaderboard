1314739b969d28fb01efd715cbd57c2b3326b6f7
john <kiokoj81@gmail.com>
2022-10-21 12:32:12 +0300

    Add time to the footer dynamically

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-import './style.css';
-import './modules/display.js';
+import "./style.css";
+import "./modules/display.js";
+
+const clock = document.getElementById("clock");
+
+const time = new Date();
+clock.innerHTML = time;
